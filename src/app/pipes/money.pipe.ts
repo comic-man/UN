f43d57e860e9d34abe15bf65a1c7ac7ae3d1112e
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-10-28 17:25:55 -0500

    Update money.pipe.ts

--- src/app/pipes/money.pipe.ts
+++ src/app/pipes/money.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MoneyPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: "name", ...args: unknown[]): unknown {
     return null;
   }
 
